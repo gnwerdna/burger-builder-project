04182057771e2ff325bb8b249fc3f7728fa38731
gnwerdna <gnwerdna.cs@gmail.com>
2019-05-24 10:44:10 +0700

    fix err connect

--- client/src/containers/Auth/Logout/Logout.js
+++ client/src/containers/Auth/Logout/Logout.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
-import {BASE_URL} from '../../../constants/abstract';
+import { BASE_URL } from "../../../constants/abstract";
 class Logout extends React.Component {
   componentDidMount() {
     fetch(BASE_URL + "/me/logout", {
       method: "post",
+      credentials: "include",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": localStorage.getItem("token")
+        Authorization: localStorage.getItem("token")
       },
       body: undefined
     }).then(res => localStorage.clear());
